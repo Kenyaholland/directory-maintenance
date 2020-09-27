2d5fb88636304b389f0dcc1d72454b9c078504d3
Kenyaholland <46533825+Kenyaholland@users.noreply.github.com>
2020-09-27 18:13:44 -0500

    Removed comments

--- main.cpp
+++ main.cpp
@@ -2,22 +2,6 @@
 #include "terminal.hpp"
 int main(){
 	Terminal* terminal = new Terminal();
-
-	/*fileSystem->ls();
-	fileSystem->pwd();
-	fileSystem->mkdir("adir");
-	fileSystem->ls();
-	fileSystem->cd("adir");
-	fileSystem->addf("f1");
-	fileSystem->addf("f3");
-	fileSystem->addf("f4");
-	fileSystem->addf("f2");
-	fileSystem->ls();
-	fileSystem->mkdir("d1");
-	fileSystem->cd("d1");
-	fileSystem->addf("f3");
-	fileSystem->addf("f2");
-	fileSystem->ls();*/
-
+	
 	return 0;	
 }
